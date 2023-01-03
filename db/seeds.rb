28bef94638abd39e5aae6897c7d221d515c49e60
Kinnari Shah <kinnari1022@yahoo.com>
2023-01-03 10:38:57 -0500

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,51 @@
+Product.delete_all
+
+Product.create!(title: 'Design and Build Great Web APIs',
+  description:
+    %{<p>
+      <em>Robust, Reliable, and Resilient</em>
+      APIs are transforming the business world at an increasing pace. Gain
+      the essential skills needed to quickly design, build, and deploy
+      quality web APIs that are robust, reliable, and resilient. Go from
+      initial design through prototyping and implementation to deployment of
+      mission-critical APIs for your organization. Test, secure, and deploy
+      your API with confidence and avoid the “release into production”
+      panic. Tackle just about any API challenge with more than a dozen
+      open-source utilities and common programming patterns you can apply
+      right away.
+      </p>},
+  image_url: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSuYTH3oYHIG9zxvTQIRYCpguB4j9_qF1M-dRZK1HStorMaLYj8jiyzz0fsOCyI3Tb-EpL8hOgERKMV6SJ_himmYswUh2zYK82lYwky0UhkVpwXjNxHJhHHYw',
+price: 24.95)
+
+Product.create!(title: 'Great Web APIs',
+  description:
+    %{<p>
+      <em>Robust, Reliable, and Resilient</em>
+      APIs are transforming the business world at an increasing pace. Gain
+      the essential skills needed to quickly design, build, and deploy
+      quality web APIs that are robust, reliable, and resilient. Go from
+      initial design through prototyping and implementation to deployment of
+      mission-critical APIs for your organization. Test, secure, and deploy
+      your API with confidence and avoid the “release into production”
+      panic. Tackle just about any API challenge with more than a dozen
+      open-source utilities and common programming patterns you can apply
+      right away.
+      </p>},
+  image_url: 'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-multi-pochette-accessoires-monogram-handbags--M44813_PM1_Worn%20view.png?wid=2048&hei=2048',
+price: 24.95)
+
+Product.create!(title: 'Design and Build',
+  description:
+    %{<p>
+      <em>Robust, Reliable, and Resilient</em>
+      APIs are transforming the business world at an increasing pace. Gain
+      the essential skills needed to quickly design, build, and deploy
+      quality web APIs that are robust, reliable, and resilient. Go from
+      initial design through prototyping and implementation to deployment of
+      mission-critical APIs for your organization. Test, secure, and deploy
+      your API with confidence and avoid the “release into production”
+      panic. Tackle just about any API challenge with more than a dozen
+      open-source utilities and common programming patterns you can apply
+      right away.
+      </p>},
+  image_url: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR1wW_WN8sIEZez0GBSiiAPK4z5okeF2GXgstCIoSAK7b9Y1x4A23k-dWay_hJF0qZYntVznmXpTVwzgF8gDoJBeLnx-TLMynx5DJOfD12XwHmkVhTvufo8n9Y')
